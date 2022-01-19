39d1c469ccc001264e60a2251b7b7c493fc62c6e
Dalton <daltonowoh@gmail.com>
2022-01-19 14:59:58 +0100

    render chat_panel

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
-import { SidePanel } from "./components/SidePanel/SidePanel.component"
+import { SidePanel } from "./components/SidePanel/SidePanel.component";
+import Chats from "./components/Chats/Chats.component";
+import { Grid } from 'semantic-ui-react';
+
+import './App.css';
 
 function App() {
   return (
-    <div>
-      <SidePanel/>
-    </div>
+    <Grid columns="equal">
+      <SidePanel />
+      <Grid.Column className="chat_panel">
+        <Chats />
+      </Grid.Column>
+      <Grid.Column width={3}>
+        <span>
+
+        </span>
+      </Grid.Column>
+    </Grid>
+
   );
 }
 
